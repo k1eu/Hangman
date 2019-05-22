7d3512beb5dcfb75390d5b9c663cdd7d3d50ce35
tkielarbb <tomek.kielar00@gmail.com>
2019-05-22 21:14:21 +0200

    - Added App Icon - Added Launchscreen with icon - Created New ViewController

---
+++ Hangman App/SoloGameVC.swift
@@ -0,0 +1,19 @@
+//
+//  SoloGameVC.swift
+//  Hangman App
+//
+//  Created by Tomasz Kielar on 22/05/2019.
+//  Copyright © 2019 Tomasz Kielar. All rights reserved.
+//
+
+import UIKit
+
+class SoloGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+
+}
